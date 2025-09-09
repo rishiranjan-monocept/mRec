1488027f885b60166158716e2211fdd74998b02a
rishiranjan-monocept <rishiranjan@monocept.com>
2025-09-09 14:33:34 +0530

    feat: integrate Redux for authentication and set up routing
    - Added Redux Toolkit for state management with an auth slice.
    - Created a global CSS file and integrated Tailwind CSS.
    - Refactored App component to use AppRoutes for routing.
    - Set up a basic Home page and routing structure.
    - Configured Redux store and root reducer.
    - Updated Vite configuration to include Tailwind CSS plugin.

---
+++ src/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./store/index.jsx";
+import "./global.css";
+import App from "./App.jsx";
+
+// import "./styles/variables.css";
+// import { registerSW } from "./registerSW";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
+
+// Register service worker
+registerSW();
