fd8721495ebfcd2c6d8c0498a0b08290af6001f7
shouryasinghsengar-monocept <shouryasinghsengar@monocept.com>
2025-09-09 18:30:53 +0530

    feat: configure environment variables and update build scripts

---
+++ src/config/env.js
@@ -0,0 +1,2 @@
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const APP_ENV = import.meta.env.VITE_ENV;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { API_BASE_URL } from "../config/env";
 
 export default function Home() {
+  const env = API_BASE_URL;
   return (
     <div>
-      <h1 className="text-red-600">Home to home</h1>
+      <h1 className="text-red-600">Home to home {env}</h1>
     </div>
   );
 }
