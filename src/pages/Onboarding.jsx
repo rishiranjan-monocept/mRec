309630aaa91c1ea124601dd441d5abedf113d7b6
rishiranjan-monocept <rishiranjan@monocept.com>
2025-09-29 13:01:37 +0530

    feat: update package.json and package-lock.json to add Prettier and remove unused dependency; enhance Onboarding page layout

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React from 'react';
 
 export default function Onboarding() {
-  return <div>Onboarding</div>;
+  return (
+    <div className="flex h-screen flex-col items-center justify-around p-4">
+      <h1 className="fill-yellow-800">Onboarding Page</h1>
+    </div>
+  );
 }
